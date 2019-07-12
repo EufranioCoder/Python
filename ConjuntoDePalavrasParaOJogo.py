aeaf1c7c3feb341fbf35c32a0bbb2e87da3a8f7a
Eufránio Diogo <44512952+Eufranio146@users.noreply.github.com>
2019-07-12 07:43:36 +0100

    Create ConjuntoDePalavrasParaOJogo.py

---
+++ ConjuntoDePalavrasParaOJogo.py
@@ -0,0 +1,21 @@
+PalavrasCom4Letras = ['fato', 'amor', 'frio', 'sede', 'vida',
+'auge', 'casa', 'rude', 'joia', 'deus', 'alma', 'novo',
+'item', 'pura', 'puro', 'luta', 'agua', 'bolo', 'pais',
+'peão']
+
+PalavrasCom5Letras = ['sagaz', 'êxito', 'nobre', 'sutil', 'termo',
+'mútua', 'ideia', 'ética', 'anexo', 'aquém',
+'genro', 'moral', 'digno', 'honra', 'muito',
+'digno', 'culto', 'ontem', 'fluir', 'louco']
+
+PalavrasCom6Letras = ['mister', 'pressa', 'nocivo', 'gentil',
+'legado', 'preços', 'dádiva', 'sessão', 'utopia', 
+'eficaz', 'hábito', 'astuto', 'paixão', 'impeto',
+'glória', 'avidez', 'alento', 'sisudo', 'rancor',
+'sanção']
+
+PalavrasCom8Letras = ['inerente', 'prudente', 'invasivo', 'respeito',
+'desgraça', 'alienado', 'prodígio', 'essência', 'abstrato',
+'conceito', 'premissa', 'verídico', 'sinônimo', 'inóspito',
+'primazia', 'resoluto', 'complexo', 'metódico', 'elegível',
+'emulação']
